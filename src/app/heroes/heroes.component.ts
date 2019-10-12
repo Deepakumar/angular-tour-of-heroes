2c44ec7c6fb5d96ed57ee110e097dbd4a3deb206
Deepakumar <gdeepakumar@mail.com>
2019-10-12 06:07:30 -0700

    Http Connection

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
+import { HeroService } from './../hero.service';
+import { Hero } from './../hero';
+import { HEROES } from './../mock-heroes';
 import { Component, OnInit } from '@angular/core';
-import { Hero} from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -7,13 +9,28 @@ import { Hero} from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id:1,
-    name: 'Windstrom'
-  }
-  constructor() { }
+  heroes: Hero[];
+  selectedHero: Hero;
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
+  getHeroes() {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 }
